5650d2a4736456d7a8922e612b4b2ee262a3c645
Rebecca Williams <70283367+rwilliams4746@users.noreply.github.com>
2020-10-17 11:43:08 -0400

    making sure commit works with java file

---
+++ src/coffeehour/CoffeeHour.java
@@ -0,0 +1,50 @@
+package coffeehour;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * CoffeeHour - Driver class
+ * 
+ * this is where JFrame is established & the only main method is
+ * (essentially the driver)
+ * 
+ * @author Rebecca Williams
+ * @version October 17, 2020
+ *
+ */
+public class CoffeeHour {
+	//global variables
+	//JFrame
+	private static JFrame f;
+	
+	//JLabel
+	private static JLabel label1;
+	private static JLabel label2;
+	
+	//JButton
+	private static JButton b;
+	
+    //JTextField
+	private static JTextField textfield;
+	
+	/**
+	 * default constructor
+	 */
+	public CoffeeHour() {
+		//default
+	}
+	
+	/**
+	 * Main method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//local variables
+		boolean isFourth; //did the user correctly input the previous three answers, and is there the fourth question
+		
+
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module coffeehour {
+	requires java.desktop;
 }
