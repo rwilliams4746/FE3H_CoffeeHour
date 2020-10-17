b6b30c37cca0f8fb2f304a44fc80bf7f875a90a8
Rebecca Williams <70283367+rwilliams4746@users.noreply.github.com>
2020-10-17 12:46:34 -0400

    completed name and house

--- src/coffeehour/CoffeeHour.java
+++ src/coffeehour/CoffeeHour.java
@@ -2,6 +2,9 @@ package coffeehour;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
 
 /**
  * CoffeeHour - Driver class
@@ -15,6 +18,9 @@ import java.awt.event.*;
  */
 public class CoffeeHour {
 	//global variables
+	private static ArrayList<FeUnit> myUnits = new ArrayList<>();
+	private static ArrayList<FeUnit> searchedUnits = new ArrayList<>();
+	
 	//JFrame
 	private static JFrame f;
 	
@@ -40,11 +46,47 @@ public class CoffeeHour {
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException{
 		//local variables
 		boolean isFourth; //did the user correctly input the previous three answers, and is there the fourth question
+		myUnits = readUnitFile("units.txt");
 		
-
+		//debugging
+		for (FeUnit feh : myUnits) {
+			System.out.println(feh.getName() + " is in house: " + feh.getHouse());
+		}
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private static ArrayList<FeUnit> readUnitFile(String filename) throws FileNotFoundException {
+		ArrayList<FeUnit> list = new ArrayList<>(); //create a list of all valid FeUnit Objects
+		String line; //the whole line
+		int spltLine; //index of comma
+		String name; //line first half, separated by comma
+		String house; //line second half
+		
+		FileReader fr = new FileReader(filename);
+        BufferedReader bfr = new BufferedReader(fr);
+		try {
+			line = bfr.readLine();
+			while (line != null) {
+				spltLine = line.indexOf(',');
+				name = line.substring(0, spltLine);
+				house = line.substring(spltLine + 1);
+				//create FeUnit Object
+				FeUnit placeholder = new FeUnit(name, house);
+				list.add(placeholder);
+				line = bfr.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			throw e;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 
 }
---
+++ src/coffeehour/FeUnit.java
@@ -0,0 +1,90 @@
+package coffeehour;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * 
+ * @author Rebecca Williams
+ * @version October 17, 2020
+ *
+ */
+public class FeUnit {
+	//global variables
+	private String name; //unit name
+	private String house; //church, golden deer, blue lions, black eagles, ashen wolves
+	private ArrayList<String> firstAnswers = new ArrayList<>();
+	private ArrayList<String> fourthQ = new ArrayList<>();
+
+	/**
+	 * constructor
+	 */
+	public FeUnit(String name, String houseAbbrev) {
+		this.name = name;
+		if (houseAbbrev.equals("blk")) {
+			house = "Black Eagles";
+		} else if (houseAbbrev.equals("blue")) {
+			house = "Blue Lions";
+		} else if (houseAbbrev.equals("gold")) {
+			house = "Golden Deer";
+		} else if (houseAbbrev.equals("seir")) {
+			house = "Church of Seiros";
+		} else if (houseAbbrev.equals("ash")) {
+			house = "Ashen Wolves";
+		} else {
+			house = "Incorrect Spelling";
+		}
+	}
+	
+	/*
+	 * getters and setters below
+	 */
+	
+	public ArrayList<String> getFirstAnswers() {
+		return firstAnswers;
+	}
+
+	public ArrayList<String> getFourthQ() {
+		return fourthQ;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getHouse() {
+		return house;
+	}
+
+	
+	public void setFirstAnswers(ArrayList<String> firstAnswers) {
+		this.firstAnswers = firstAnswers;
+	}
+	
+	public void setFourthQ(ArrayList<String> fourthQ) {
+		this.fourthQ = fourthQ;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/*
+	 * using abbreviation of house name, determine the house
+	 */
+	public void setHouse(String houseAbbrev) {
+		if (houseAbbrev.equals("blk")) {
+			house = "Black Eagles";
+		} else if (houseAbbrev.equals("blue")) {
+			house = "Blue Lions";
+		} else if (houseAbbrev.equals("gold")) {
+			house = "Golden Deer";
+		} else if (houseAbbrev.equals("seir")) {
+			house = "Church of Seiros";
+		} else if (houseAbbrev.equals("ash")) {
+			house = "Ashen Wolves";
+		} else {
+			house = "Incorrect Spelling";
+		}
+	}
+}
